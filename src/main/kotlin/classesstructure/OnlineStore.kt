c09eda5881e1e6881f754c108257d2a54541c2b2
MiguelY <miguelyeac@hotmail.com>
2023-10-20 16:46:07 -0300

    exampleStore

---
+++ src/main/kotlin/classesstructure/OnlineStore.kt
@@ -0,0 +1,45 @@
+package classesstructure
+
+class OnlineStore {
+
+    constructor() {
+        userName = "testUserName"
+        balance = 20
+    }
+
+    constructor(paramUSer: String) {
+        userName = paramUSer
+        balance = 80
+    }
+
+    constructor(paramUSer: String, paramBalance: Int) {
+        userName = paramUSer
+        balance = paramBalance
+    }
+
+    var userName = "default"
+    var balance = 0
+    val shirtValue = 20
+
+    fun canPurchase() {
+        if (balance >= shirtValue) {
+            val howMuch = (balance / shirtValue)
+            when (howMuch) {
+                1 -> { println("el usuario $userName puede comprar solo $howMuch camisa") }
+                else -> { println("el usuario $userName puede comprar $howMuch camisas") }
+            }
+        }
+    }
+
+}
+
+fun executeOnlinteStore() {
+    val storeBasic = OnlineStore()
+    val storeWithName = OnlineStore("tienda Loca")
+    val storeOnline = OnlineStore("tienda mascotas", 120)
+
+    storeBasic.canPurchase()
+    storeWithName.canPurchase()
+    storeOnline.canPurchase()
+
+}
