4c314e07fc4e24ae3baf143726898371191d0fc8
MiguelY <miguelyeac@hotmail.com>
2024-06-03 22:14:08 -0300

    example standard functions

---
+++ src/main/kotlin/standardfun/AATheory.kt
@@ -0,0 +1,22 @@
+package standardfun
+
+/*
+
+Standard functions
+
+scope functions
+execute in code in the context of an object
+creates a temporary scope while the code is executed
+
+
+let - execute a block of code, when the object is NOT NULL - lambda function
+with
+run
+apply
+also
+
+
+takeIF
+takeUnless
+
+ */
