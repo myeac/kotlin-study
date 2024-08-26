bf3940118d6ad0012cff148e5f168e7fd728803e
MiguelY <miguelyeac@hotmail.com>
2024-08-26 20:28:24 -0300

    generics theory

---
+++ src/main/kotlin/generics/AATheory.kt
@@ -0,0 +1,16 @@
+package generics
+
+/*
+
+Generics
+type constraints'
+
+a class can have type parameters
+the type can be used by variables and methods
+useful when ca class can handle multiple types of parameters
+
+ */
+
+val animals: ArrayList<String> = arrayListOf("cat", "dog", "mouse")
+// String es el valor generico, porque puede ser INT, FLOAT, etc
+
