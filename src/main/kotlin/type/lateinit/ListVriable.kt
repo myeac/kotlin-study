0195359165fc730d6748c6c357c4d733ffdbb52a
MiguelY <miguelyeac@hotmail.com>
2024-08-26 22:05:32 -0300

    adjust lateinit list

--- src/main/kotlin/type/lateinit/ListVriable.kt
+++ src/main/kotlin/type/lateinit/ListVriable.kt
@@ -3,7 +3,7 @@ package type.lateinit
 lateinit var message: String
 
 fun executeListVariable() {
-    var list = listOf(1, 2, 3, 51, ,3, 5, 2, 1, 6, 67, 78, 34, 3, 1)
+    var list = listOf(1, 2, 3, 51, 4, 3, 5, 2, 1, 6, 67, 78, 34, 3, 1)
     message = "The chosen prime number is ${selectNumber(list)}"
 }
 
